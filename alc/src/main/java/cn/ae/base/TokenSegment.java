734f1c655a9df6d15330923a101b261da43d7b27
白子煦 <xxm17808@163.com>
2026-03-24 09:18:56 +0000

    update alc/src/main/java/cn/ae/base/TokenSegment.java.
    Signed-off-by: 白子煦 <xxm17808@163.com>

--- alc/src/main/java/cn/ae/base/TokenSegment.java
+++ alc/src/main/java/cn/ae/base/TokenSegment.java
@@ -1,1 +1,9 @@
 package cn.ae.base
+import java.util.ArrayList;
+import java.util.List;
+public class TokenSegment{
+    public static class Type{
+        public static final int string=0;
+        public static final int num=1;
+    }
+}
